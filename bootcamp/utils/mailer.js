bc9229b8785402225c3862ffc7e420407792e8e4
hokagecreativelabs <hokagecreativelabs001@gmail.com>
2025-07-17 12:03:58 +0100

    gg

---
+++ bootcamp/utils/mailer.js
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+
+export const sendMail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: `"Hokage Creative Labs" <${process.env.MAIL_USER}>`,
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return { success: true, info };
+  } catch (err) {
+    return { success: false, error: err.message };
+  }
+};
