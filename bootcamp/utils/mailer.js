a96a238b46fc457b792f585a7fc858cee9e4f772
hokagecreativelabs <hokagecreativelabs001@gmail.com>
2025-07-20 23:06:53 +0100

    gg

--- bootcamp/utils/mailer.js
+++ bootcamp/utils/mailer.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 const sendMail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
